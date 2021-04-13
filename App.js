e30c5c14b1e62de892dc9b0d952c25a308af579e
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-13 19:48:12 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import NavBar from './NavBar';
 import Header from './Header';
+
+import ContextAPI from './ContextAPI';
+
 import PortfolioSection from './PortfolioSection';
 import About from './About';
 import ContactMe from './ContactMe';
@@ -12,6 +15,7 @@ function App() {
   <div className="App">
        <NavBar/>
           <Header/>
+          <ContextAPI/>
           <PortfolioSection/>
           <About/>
           <ContactMe/>
