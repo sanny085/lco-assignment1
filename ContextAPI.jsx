b87b8c9359f903258c12383e4ba674f729c78469
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-14 19:42:09 +0530

    Update ContextAPI.jsx

--- ContextAPI.jsx
+++ ContextAPI.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import 
+import Provider from './provider'
+
 
 function ContextAPI() {
     return (
         <React.Fragment>
           <section class="page-section portfolio" id="portfolio">
             <div class="container">
-            
+               <Provider>
+                   <Agents/>
+               <Provider>
+  
             </div> 
           </section>
         </React.Fragment>
