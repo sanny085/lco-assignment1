04ee0396f333eab707956499a33609aa5a033d01
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-16 17:08:01 +0530

    Add files via upload

---
+++ Color.js
@@ -0,0 +1,14 @@
+const AppTheme = {
+    light: {
+      backgroundColor: "#CAD5E2",
+      textColor: "#000000",
+      border: "2px solid #2c3e50"
+    },
+    black: {
+      backgroundColor: "#242B2E",
+      textColor: "#FFFFFF",
+      border: "2px dashed #1abc9c"
+    }
+};
+
+export default AppTheme;
