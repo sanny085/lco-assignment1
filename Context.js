dbb493850ca5c48931b3e08e740efe5519fc733b
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-16 16:12:02 +0530

    Update Context.js

--- Context.js
+++ Context.js
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
+import  { createContext } from 'react';
 
-const StateContext = createContext();
+const StateContext = createContext(["light", () =>{}]);
 
 export default StateContext;
