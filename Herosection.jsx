ae5a8d068e27b3a2cffbb9d8f98467c607ff1b7f
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-18 12:40:29 +0530

    Update Herosection.jsx

--- Herosection.jsx
+++ Herosection.jsx
@@ -1,21 +1,30 @@
-import React, {useContext} from 'react';
+import React, { useContext, useState } from 'react';
 
 import StateContext from './Context';
 import AppTheme from './Color';
 
 const Herosection = () => {
-    const theme = useContext(StateContext) [0];
+    const theme = useContext(StateContext) [0] ;
     const currTheme = AppTheme[theme]; 
+
+    const [change, setChange] = useState({currTheme});
+    
+   console.log(`${change.currTheme.backgroundColor}`+" this 89");
+   
     return(
-        <div>
+        <div style={{ padding: "1rem",
+                      backgroundColor: `${change.currTheme.backgroundColor}`,
+                      color: `${change.currTheme.textColor}`,
+                      textAlign: "center",
+                      border:`${change.currTheme.border}`
+                      }} >
             <h1>Context API Theme Toggler</h1>
             <p>This is dummy text to check dark mode</p>
 
-            <button style={{backgroundColor:"#226ae60",
+            <button onClick={ () => {setChange(change.currTheme === "light" ? "dark" : "light")} } style={{backgroundColor:"green",
                             padding:"10px 150px",
                             fontSize: "20px",
-                            color: "#fff",
-                            border:`${currTheme.border}`
+                            color: "#fff"
             }}>
             Click Me</button>
         </div>
