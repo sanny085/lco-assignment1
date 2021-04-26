b864ef348de4b5b6e131a32b4a3ead9c3bc828b5
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-26 14:57:37 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './App.css';
 import NavBar from './NavBar';
 import Header from './Header';
-
-import ContextAPI from './ContextAPI';
+ 
 import DarkLight from './DarkLight';
 
 import PortfolioSection from './PortfolioSection';
@@ -12,12 +11,11 @@ import ContactMe from './ContactMe';
 import Footer from './Footer';
 
 function App() {
-  return (
+  return ( 
   <div className="App">
        <NavBar/>
           <Header/>
-          
-          <ContextAPI/>
+           
           <DarkLight/>
           
           <PortfolioSection/>
