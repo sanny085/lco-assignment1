296f964cc1bf894ed2dc2d2c6b5d71ccb7530f55
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-26 14:58:50 +0530

    Update Herosection.jsx

--- Herosection.jsx
+++ Herosection.jsx
@@ -2,23 +2,23 @@ import React, { useContext, useState } from 'react';
 
 import StateContext from './Context';
 import AppTheme from './Color';
+import ContextAPI from './ContextAPI';
 
 const Herosection = () => {
     const [themeMode, setThemeMode] = useContext(StateContext);
-
     const theme = useContext(StateContext)[0] ;
     const currTheme = AppTheme[theme]; 
-
-    const [change, setChange] = useState({currTheme});
-    
-   console.log(`${change.currTheme.backgroundColor}`+" this 89");
+  
     return(
+        <>
+        <ContextAPI />
         <div style={{ padding: "1rem",
                       backgroundColor: `${currTheme.backgroundColor}`,
                       color: `${currTheme.textColor}`,
                       textAlign: "center",
                       border:`${currTheme.border}`
-                      }} >
+                      }}>
+        
             <h1>Context API Theme Toggler</h1>
             <p>This is dummy text to check dark mode</p>
 
@@ -29,6 +29,10 @@ const Herosection = () => {
             }}>
             Click Me</button>
         </div>
+
+     
+        </>
+       
     )
 }
 export default Herosection;
