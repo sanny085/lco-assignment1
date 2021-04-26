ad13bbc89c6546e6d48ff77e5e86c43d15f29260
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-26 14:58:31 +0530

    Update ThemeToggler.jsx

--- ThemeToggler.jsx
+++ ThemeToggler.jsx
@@ -7,19 +7,18 @@ const ThemeToggler = () => {
    const [themeMode, setThemeMode] = useContext(StateContext);
 
    const [isDarkMode, setIsDarkMode] = useState(() => false);
-   return (
 
-        <div className="text-center" onClick={ () => {setThemeMode(themeMode === "light" ? "dark" : "light")} }>
-            <DarkModeToggle onChange={setIsDarkMode} checked={isDarkMode} size={85} speed={10} />
-            
-            <span>{themeMode === "light" ? "Light off" : "Light on"}</span>
-       
-           {
-               console.log(" Toggle : "+themeMode)
-           }
-               <br/><br/>
-        </div>
-        
+   
+   return (
+      
+     <div className="text-center">
+      <br/>
+        <span onClick={ () => {setThemeMode(themeMode === "light" ? "dark" : "light")} }>
+            <DarkModeToggle  className="btnOutline" onChange={setIsDarkMode} checked={isDarkMode} size={85} speed={10} />
+               { console.log(" Toggle : "+themeMode) }
+        </span>
+      <br/> <br/>
+     </div>
     );
 };
 export default ThemeToggler;
