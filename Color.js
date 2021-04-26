0d88889a86c3d26e421a8e7151d3750d8b7bea45
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-26 14:59:37 +0530

    Update Color.js

--- Color.js
+++ Color.js
@@ -1,13 +1,13 @@
 const AppTheme = {
     light: {
       backgroundColor: "#CAD5E2",
-      textColor: "#000000",
+      textColor: "#24292e",
       border: "2px dashed #1abc9c"
       
     },
     dark: {
-      backgroundColor: "#242B2E",
-      textColor: "#FFFFFF",
+      backgroundColor: "#22272e",
+      textColor: "#adbac7",
       border: "2px solid #2c3e50"
     }
 };
