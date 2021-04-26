0248a442287e0e69afbdf47764822aed8e8d9e36
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-26 13:21:19 +0530

    Add files via upload

---
+++ DarkLight.jsx
@@ -0,0 +1,22 @@
+import React, {useState}  from 'react'
+
+import StateContext from './Context';
+import ThemeToggler from './ThemeToggler';
+import Herosection from './Herosection';
+
+const DarkLight = ()=> {
+    const themeHook = useState("dark");
+ 
+    return (
+        <div>
+            {/*Dark & light Mode*/}
+            <StateContext.Provider value={themeHook}>  
+               <ThemeToggler/>
+               <Herosection/>
+             </StateContext.Provider>
+            {/*End Dark & light Mode*/}
+        </div>
+    )
+}
+export default DarkLight;
+
